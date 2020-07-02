3525ab10d4d29f23e58220a3e69fba5f253753bc
KKutov <67765278+KKutov@users.noreply.github.com>
2020-07-03 02:06:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Zadacha5
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] colors = { 'b', 'r', 'w', 'b', 'r', 'b', 'w', 'r', 'w', 'b', 'b', 'r' };
+            int red = 0;
+            int blue = colors.Length - 1;
+
+            for (int i = 0; i <= blue; i++)
+            {
+                if (red < blue)
+                {
+                    if (colors[i] == 'r')
+                    {
+                        char temp = colors[i];
+                        colors[i] = colors[red];
+                        colors[red] = temp;
+                        red++;
+                    }
+                    else if (colors[i] == 'w')
+                    {
+                        char temp = colors[i];
+                        colors[i] = colors[blue];
+                        colors[blue] = temp;
+                        blue--;
+                        i--;
+                    }
+                }
+            }
+
+            Console.WriteLine("Results: ");
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Console.Write($"{colors[i]} ");
+            }
+        }
+    }
+}
